afe49cfdb691fe148f958370b31ae6cc174f27a2
Othmar Gispert <ogisq@allstate.com>
2022-09-23 13:07:37 -0500

    Initial Commit

---
+++ RidaApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RidaApp
+//
+//  Created by ogisq on 9/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RidaApp/RidaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RidaAppApp.swift
+//  RidaApp
+//
+//  Created by ogisq on 9/23/22.
+//
+
+import SwiftUI
+
+@main
+struct RidaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
